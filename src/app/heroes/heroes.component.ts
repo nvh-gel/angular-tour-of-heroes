fa7cb973cbe3f36a9f56ce10cc3f89e3a57faf54
nvh-gel <nvhien2703@outlook.com>
2021-08-30 23:15:55 +0700

    list heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HEROES} from "../mock-heroes";
 import {Hero} from "../hero";
 
 @Component({
@@ -8,14 +9,16 @@ import {Hero} from "../hero";
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
+  heroes = HEROES;
+  selectedHero?: Hero;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,19 @@
+import {Hero} from "./hero";
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Iron Man'},
+  {id: 12, name: 'Thor'},
+  {id: 13, name: 'Dr Strange'},
+  {id: 14, name: 'Hulk'},
+  {id: 15, name: 'Wolverine'},
+  {id: 16, name: 'Vision'},
+  {id: 17, name: 'Hawkeye'},
+  {id: 18, name: 'Captain America'},
+  {id: 19, name: 'Black widow'},
+  {id: 20, name: 'Black panther'},
+  {id: 21, name: 'Deadpool'},
+  {id: 22, name: 'Captain Marvel'},
+  {id: 23, name: 'Loki'},
+  {id: 24, name: 'Antman'},
+  {id: 25, name: 'SpiderMan'},
+]
